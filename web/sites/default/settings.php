f79658ae207af48ae5470f8389cdbbdd586f577b
Adeline <adeline.niedbalski@actency.com>
2023-10-17 12:05:51 +0200

    Décharge

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,12 +1,13 @@
 <?php
 
+
 /**
 * Default settings file.
 **/
 if (file_exists($app_root . '/sites/default/default.settings.php')) {
   include $app_root . '/sites/default/default.settings.php';
 }
-  
+
 /**
 * Application specific settings file.
 **/
@@ -14,7 +15,17 @@ $app_main = explode('/', $app_root);
 array_pop($app_main);
 $app_main = implode('/', $app_main);
 
+
 if (file_exists($app_main . '/config/drupal/settings.app.php')) {
   include $app_main . '/config/drupal/settings.app.php';
 }
-  
+
+if (file_exists($app_main . '/config/drupal/settings.local.php')) {
+  include $app_main . '/config/drupal/settings.local.php';
+}
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
---
+++ web/themes/custom/acten_blog_theme/js/acten-blog-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Acten Blog Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.actenBlogTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
